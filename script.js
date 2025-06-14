a3cbe8b6a08bb36af3530ed19dc45e6cfbcf5d64
Vipin Yadav <76983380+yvipin6887@users.noreply.github.com>
2025-06-14 22:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function toCase(text) {
   // write your code here
+	if (text == '') return '-';
+
+	return `${text.toLowerCase()}-${text.toUpperCase()}`;
 }
 
 // DO not change the code below
 
 const text = prompt("Enter text:");
+
 alert(toCase(text));
